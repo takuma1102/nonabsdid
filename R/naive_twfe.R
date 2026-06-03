cc9d706535cbe5d7f6257fa96de2b17338f06f6d
Takuma Iwasaki <94999658+takuma1102@users.noreply.github.com>
2026-06-03 04:55:58 -0700

    Update naive_twfe.R

--- R/naive_twfe.R
+++ R/naive_twfe.R
@@ -43,7 +43,7 @@
 #' what makes it useful as a visual baseline.
 #'
 #' @examples
-#' \donttest{
+#' \dontrun{
 #'   ref <- naive_twfe(mydata, outcome = "y", treatment = "d",
 #'                     unit = "id", time = "t",
 #'                     lags = 12, leads = 6)
